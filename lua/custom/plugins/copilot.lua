8fe9e7f9f7dd547aae3a76704dd922133756ed46
Eric Goddard <3970088+egoddard@users.noreply.github.com>
2024-05-10 08:00:53 -0500

    Use lua-based copilot and add copilot chat

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,10 +1,19 @@
 return {
-  'github/copilot.vim',
-  config = function()
-    vim.keymap.set('i', '<C-y>', 'copilot#Accept("\\<CR>")', {
-      expr = true,
-      replace_keycodes = false,
-    })
-    vim.g.copilot_no_tab_map = true
-  end,
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      }
+    end,
+  },
+  {
+    'zbirenbaum/copilot-cmp',
+    config = function()
+      require('copilot_cmp').setup()
+    end,
+  },
 }
